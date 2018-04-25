953be190e2237e6bd72c36ca026013d458c859e9
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2018-04-25 08:58:02 +0200

    Added GPL header

--- src/rserver.cpp
+++ src/rserver.cpp
@@ -1,3 +1,20 @@
+/*
+ * This file is part of mapping-r-server (https://github.com/umr-dbs/mapping-r-server).
+ * Copyright (c) 2018 Database Research Group of the University of Marburg.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #define RSERVER_FORKED_MODE true // specify whether the server runs forked or threaded
 
 #include "util/exceptions.h"
