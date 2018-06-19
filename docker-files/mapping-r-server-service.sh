0fb4090f5d9bc1c01bffe0a027a35fb1127948dd
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2018-06-19 13:31:52 +0200

    fixed r-server service path

--- docker-files/mapping-r-server-service.sh
+++ docker-files/mapping-r-server-service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /app
-exec /sbin/setuser www-data /app/mapping-r-server/target/bin/r_server >> /var/log/mapping-r-server.log 2>&1
+exec /sbin/setuser www-data /app/r_server >> /var/log/mapping-r-server.log 2>&1
